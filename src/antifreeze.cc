beb4cce739c8438bed7fde7c7dfbfc93af290339
MESYETI <deedlebum41@gmail.com>
2022-01-31 20:05:59 +0000

    make antifreeze wait 6 seconds before killing

--- src/antifreeze.cc
+++ src/antifreeze.cc
@@ -9,7 +9,9 @@ void antifreeze(uint16_t& freezetime, UI::Window& textbox, bool& run) {
 	freezewin.y = 0;
 	freezewin.colour_pair = COLOUR_PAIR_BAR;
 	freezewin.title = "yedit has frozen";
-	freezewin.contents = "yedit has frozen\nplease report this to MESYETI and tell him how\nto reproduce this error, press space to continue\nto yedit (your work has been saved)";
+	freezewin.contents = "yedit has frozen\nplease report this to MESYETI and tell him how\nto reproduce this error, press space to kill yedit";
+	int16_t killTimer = 6000;
+
 	while (run) {
 		if (freezetime > 3000) {
 			textbox.textboxInput         = "";
@@ -17,15 +19,22 @@ void antifreeze(uint16_t& freezetime, UI::Window& textbox, bool& run) {
 			while (getch() != ' ') {
 				freezewin.w = COLS;
 				freezewin.h = LINES;
-				clear();
 				freezewin.Render();
 				refresh();
-				if (getch() == 'q') {
+				freezewin.contents = "yedit has frozen\n"
+				"please report this to MESYETI and tell him how\n"
+				"to reproduce this error\n"
+				"press space to kill yedit or wait for the kill timer to end\n\n"
+				"killing in " + to_string((int) (killTimer / 1000)) + " seconds";
+				killTimer -= 1000/25;
+				if (killTimer <= 0) {
 					IOHandle::Exit();
 					exit(1);
 				}
-				usleep(1000000/60);
+				usleep(1000000/25);
 			}
+			IOHandle::Exit();
+			exit(1);
 		}
 		freezetime += 1000/25;
 		usleep(1000000/25);
